4ab1f3d84ff5ccdfccf7ce71568d2c074ba0f585
komalvsingh <komalvsingh1111@gmail.com>
2026-04-11 16:50:07 +0530

    bc1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.VITE_API_URL;
 
 export const getDecision = async () => {
   const res = await fetch(`${API_URL}/decision`);
